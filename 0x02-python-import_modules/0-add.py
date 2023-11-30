bc9881e678448cb0b392f58984f56aa4269ff7fc
orjimart <orjimart@gmail.com>
2023-11-30 07:11:55 -0800

    python task 3 update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,1 +1,7 @@
-__import__("0-add")
+#!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
-    add = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, add))
-
-    sub = sub(a, b)
-    print("{:d} - {:d} = {:d}".format(a, b, sub))
-
-    mul = mul(a, b)
-    print("{:d} * {:d} = {:d}".format(a, b, mul))
-
-    div = div(a, b)
-    print("{:d} / {:d} = {:.0f}".format(a, b, div))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
