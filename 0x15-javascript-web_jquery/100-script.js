05e233cbb384eb16bb16f967b54ce0090aa97d36
root <orjimart@gmail.com>
2024-06-11 16:28:14 -0700

    0x15-javascript web jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Updates updates the text color of the <header> element to red
+// using use document.querySelector to select the HTML tag
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
